cb6a44c2ff9f0934e61b689e8119a579647356bb
st12n <stijn.schouppe@hotmail.com>
2024-05-17 12:33:20 +0200

    Added white clickable thingies

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ import (
 	"golang.org/x/image/colornames"
 )
 
+type board struct{}
+
 func loadPicture(path string) (pixel.Picture, error) {
 	file, err := os.Open(path)
 	if err != nil {
@@ -40,11 +42,17 @@ func run() {
 	}
 
 	pic, err := loadPicture("cross.png")
-	if err != nil {
+	//blackpic, berr := loadPicture("black-tile.png")
+	whitepic, werr := loadPicture("white-tile.png")
+	if err != nil || werr != nil {
 		panic(err)
 	}
 
 	cross := pixel.NewSprite(pic, pixel.R(0, 0, 50, 50))
+
+	//blacktile := pixel.NewSprite(blackpic, pixel.R(0, 0, 50, 50))
+	var gamematrix []pixel.Matrix
+	var tiles []*pixel.Sprite
 
 	for !win.Closed() {
 
@@ -54,6 +62,15 @@ func run() {
 			for y := 50; y < boardbounds; y += 50 {
 				cross.Draw(win, pixel.IM.Moved(pixel.Vec{float64(x), float64(y)}))
 			}
+		}
+		for i, tile := range tiles {
+			tile.Draw(win, gamematrix[i])
+		}
+		if win.JustPressed(pixelgl.MouseButtonLeft) {
+			whitetile := pixel.NewSprite(whitepic, pixel.R(0, 0, 50, 50))
+			tiles = append(tiles, whitetile)
+			mouse := win.MousePosition()
+			gamematrix = append(gamematrix, pixel.IM.Scaled(pixel.ZV, 1).Moved(mouse))
 		}
 
 		win.Update()
