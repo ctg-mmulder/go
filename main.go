48c1b36bdac360a18b3809d86e817ad1b63ff375
Michiel Mulder <michiel.mulder@ctg.com>
2024-05-17 10:30:06 +0200

    feat: use pixel for window and rendering

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gopxl/pixel"
+	"github.com/gopxl/pixel/pixelgl"
+)
+
+func run() {
+	cfg := pixelgl.WindowConfig{
+		Title:  "Pixel Rocks!",
+		Bounds: pixel.R(0, 0, 1024, 768),
+	}
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	for !win.Closed() {
+		win.Update()
+	}
+}
 
 func main() {
-	fmt.Println("Let's Go!")
+	pixelgl.Run(run)
 }
